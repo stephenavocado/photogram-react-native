428841c05bb5b855ac2ad94f6cb8089314a30bfd
stephenavocado <stephenwalter@chicagobooth.edu>
2023-02-19 23:00:48 +0000

    photo styles

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
+import { sample_photos } from "./sample_photos/photos";
+import { AntDesign } from '@expo/vector-icons';
 
 function Photo({ photo }) {
+    const sampleImage = sample_photos.find(sample_photo => sample_photo.id === photo.image);
+
     return (
         <View style={styles.photo}>
             <Text style={styles.username}>{photo.owner.username}</Text>
-            <Image 
-                style={styles.image}
-                source={{ 
-                    uri: photo.url
-                }}
-            />
-            <Text>{photo.likes_count} {photo.likes_count === 1 ? "like" : "likes"}</Text>
+            <View style={styles.imageWrapper}>
+                <Image 
+                    style={styles.image}
+                    source={sampleImage.src}
+                />
+            </View>
+            <View style={styles.likes}>
+                <AntDesign name="hearto" color="deeppink" />
+                <Text style={styles.likesCount}>
+                    {photo.likes_count} {photo.likes_count === 1 ? "like" : "likes"}
+                </Text>
+            </View>
             <Text style={styles.caption}>{photo.caption}</Text>
         </View>
     )
@@ -19,18 +28,32 @@ function Photo({ photo }) {
 
 const styles = StyleSheet.create({
     photo: {
-        padding: 20,
+        padding: 16,
         marginVertical: 8,
         borderWidth: 1,
         borderRadius: 8,
-        borderColor: 'slategrey'
+        borderColor: 'gainsboro',
+        width: '100%'
+    },
+    imageWrapper: {
+        height: 225,
+        marginVertical: 12,
+        marginLeft: -16,
+        marginRight: -16,
     },
     image: {
-        height: 200,
-        width: '100%'
+        height: '100%',
+        width: '100%',
     },
     username: {
         fontWeight: '700'
+    },
+    likes: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    likesCount: {
+        marginLeft: 6,
     },
     caption: {
         marginTop: 12,
